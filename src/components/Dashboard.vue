ae2baed1bb2ed90550da24e5b8e1621c41731151
callum buttery <callumbuttery@icloud.com>
2022-05-15 17:48:28 +0100

    Feat: initial files

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="ma-1 pa-1">
+    <v-card>
+      <v-navigation-drawer permanent expand-on-hover>
+        <user-info />
+
+        <v-divider></v-divider>
+
+        <v-list nav dense>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-folder</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Files</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Shared with me</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-star</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Starred</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import UserInfo from './UserInfo.vue';
+
+export default {
+    components: {
+        UserInfo,
+    }
+}
+</script>
