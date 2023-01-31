25880ec2e0a8d39dcf935cea9c26187940478bb0
callum buttery <callumbuttery@icloud.com>
2023-01-31 20:46:16 +0000

    Fix: add in router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
 import { Login } from "./Pages/Login";
+import { Home } from "./Pages/Home";
 
-
-export const App: React.FC= ()=> {
+export const App: React.FC = () => {
   return (
     <div className="App">
-      <Route path="/" element={<Login/>} />
-      <Route path="/products/:id" element={<Login/>} />
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,8 @@
+export const Home: React.FC = () => {
+
+  return (
+    <section className="h-screen bg-no-repeat bg-cover bg-center bg-fixed flex justify-center bg-main md:h-screen">
+      This is home
+    </section>
+  );
+};
