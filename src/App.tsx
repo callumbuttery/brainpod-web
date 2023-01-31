fb311119e1734e88432b927e7b85260520067dbf
callum buttery <callumbuttery@icloud.com>
2023-01-31 20:38:24 +0000

    Feat: add in router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
-import { Login } from './Pages/Login';
+import { Login } from "./Pages/Login";
 
-export const App: React.FC = () => {
+
+export const App: React.FC= ()=> {
   return (
     <div className="App">
-      <Login/>
+      <Route path="/" element={<Login/>} />
+      <Route path="/products/:id" element={<Login/>} />
+      <Login />
     </div>
   );
-}
+};
 
 export default App;
